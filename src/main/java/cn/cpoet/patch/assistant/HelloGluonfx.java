ab0bd6884743fb0e70d983e0674768a11af68cb8
CPoet <llzero54@foxmail.com>
2025-11-22 21:16:58 +0800

    feat: init project

---
+++ src/main/java/cn/cpoet/patch/assistant/HelloGluonfx.java
@@ -0,0 +1,10 @@
+package cn.cpoet.patch.assistant;
+
+/**
+ * @author CPoet
+ */
+public class HelloGluonfx {
+    public static void main(String[] args) {
+        System.out.println("Hello Gluonfx!");
+    }
+}
